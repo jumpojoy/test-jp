9ce9cadd128d88532a663c4f7731dff21359e8c9
Vasyl Saienko <vsaienko@mirantis.com>
2017-06-27 18:19:54 +0300

    update

--- test.groovy
+++ test.groovy
@@ -6,7 +6,7 @@ timestamps {
   node("master"){
     ssh.prepareSshAgentKey(CREDENTIALS_ID)
     ssh.ensureKnownHosts(GERRIT_HOST)
-    ssh.agentSh(String.format("ssh -p %s@%s id", GERRIT_NAME, GERRIT_HOST))
+    ssh.agentSh(String.format("ssh %s@%s id", GERRIT_NAME, GERRIT_HOST))
     sh("id")
   }
 }
