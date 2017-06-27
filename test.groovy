25c134b28ae54b28bc1197a0690af7192f79428d
Vasyl Saienko <vsaienko@mirantis.com>
2017-06-27 18:11:18 +0300

    update

--- test.groovy
+++ test.groovy
@@ -1,8 +1,12 @@
 def common = new com.mirantis.mk.Common()
 def git = new com.mirantis.mk.Git()
+def ssh = new com.mirantis.mk.Ssh()
 
 timestamps {
   node("master"){
+    ssh.prepareSshAgentKey(CREDENTIALS_ID)
+    ssh.ensureKnownHosts(GERRIT_HOST)
+    ssh.agentSh(String.format("ssh -p %s@%s id", GERRIT_NAME, GERRIT_HOST))
     sh("id")
   }
 }
